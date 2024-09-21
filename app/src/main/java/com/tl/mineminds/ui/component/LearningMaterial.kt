d6cb5088ecb88abd9f67b406022eab617d99f0e2
icefire <icefire619@gmail.com>
2024-09-21 17:01:32 +0530

    refactor: Beautify profile

--- app/src/main/java/com/tl/mineminds/ui/component/LearningMaterial.kt
+++ app/src/main/java/com/tl/mineminds/ui/component/LearningMaterial.kt
@@ -1,21 +1,28 @@
 package com.tl.mineminds.ui.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.tl.mineminds.R
 import com.tl.mineminds.entity.LearningMaterial
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun LearningMaterial(learningMaterial: LearningMaterial) {
-    Column {
+    val scrollable = rememberScrollState()
+    Column(modifier = Modifier.verticalScroll(scrollable)) {
         Text(text = learningMaterial.title)
         Image(painter = rememberImagePainter(R.drawable.login_illustration), contentDescription = "", contentScale = ContentScale.Crop, modifier = Modifier
             .fillMaxWidth()
--- app/src/main/java/com/tl/mineminds/ui/screen/MainScreen.kt
+++ app/src/main/java/com/tl/mineminds/ui/screen/MainScreen.kt
@@ -1,14 +1,25 @@
 package com.tl.mineminds.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.tl.mineminds.MainViewModel
+import com.tl.mineminds.R
 
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
@@ -17,7 +28,13 @@ fun MainScreen(viewModel: MainViewModel) {
         when(mainScreenRoute) {
             ScreenNames.SUBJECTS.routeName -> {
                 val subjects by viewModel.subjects.observeAsState()
-                SubjectScreen(subjects!!, viewModel)
+                Column {
+                    Image(painter = rememberImagePainter(R.drawable.profile_dummy2), contentDescription = "", contentScale = ContentScale.Crop, modifier = Modifier
+                        .fillMaxWidth()
+                        .height(380.dp))
+                    SubjectScreen(subjects!!, viewModel)
+                }
+
             }
             ScreenNames.LESSONS.routeName -> {
                 LessonScreen(lessonItems = emptyList(), viewModel)
